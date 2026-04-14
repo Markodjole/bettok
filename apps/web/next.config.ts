9851e418d975c718d99d3e065474cbc001960e39
Markodjole <marko.djordjevickg@gmail.com>
2026-04-14 15:29:51 +0200

    fiiiix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/*": ["./bin/ffmpeg"],
+  },
   transpilePackages: [
     "@bettok/types",
     "@bettok/core",
